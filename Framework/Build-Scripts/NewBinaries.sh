be215066a8af0ab1fa177cb43e2975f670003488
eplrepo <46343380+eplrepo@users.noreply.github.com>
2019-01-18 18:48:47 +0530

    Update NewBinaries.sh

--- Framework/Build-Scripts/NewBinaries.sh
+++ Framework/Build-Scripts/NewBinaries.sh
@@ -1,15 +1,15 @@
 echo "Copying files from Host to target"
-echo "zImage"
+echo "Copying new zImage...."
 sshpass -p "epl@123" scp epl@192.168.0.2:/home/epl/LTIB/LTP_Source/home/epl/LTPbuildroot/pi2/zImage /tmp/                 
-echo "bcm2709-rpi-2-b.dtb"
+echo "Copying new Device Tree...."
 sshpass -p "epl@123" scp epl@192.168.0.2:/home/epl/LTIB/LTP_Source/home/epl/LTPbuildroot/pi2/bcm2709-rpi-2-b.dtb /tmp/                 
-echo "rootfs.ext4"
+echo "Copying new Filesystem..."
 sshpass -p "epl@123" scp epl@192.168.0.2:/home/epl/LTIB/LTP_Source/home/epl/LTPbuildroot/pi2/rootfs.ext4 /tmp/                 
 
-echo "Check for New SW"
+echo "Verify Checksum..."
 if [[ -f /tmp/zImage && -f /tmp/bcm2709-rpi-2-b.dtb && -f /tmp/rootfs.ext4 ]];then 
-		echo "All required files are present\n"
+		echo "Checksum Passed...\n"
 		./SwUpg.sh
 	else
-		echo "All required files are not present\n"
+		echo "Checksum Failed...\n"
 fi
