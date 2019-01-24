4a91246b79921d600cbe742e8b5d7bd89855bdd9
purushotham447 <srinivas_r@hcl.com>
2019-01-24 13:08:47 +0530

    Updated ip address in respective files for SW Upgrade

--- Framework/Build-Scripts/NewBinaries.sh
+++ Framework/Build-Scripts/NewBinaries.sh
@@ -1,10 +1,10 @@
 echo "Copying files from Host to target"
 echo "Copying new zImage...."
-sshpass -p "epl@123" scp epl@192.168.0.2:/home/epl/LTIB/LTP_Source/home/epl/LTPbuildroot/pi2/zImage /tmp/                 
+sshpass -p "epl@123" scp epl@10.144.172.149:/home/epl/LTIB/LTP_Source/home/epl/LTPbuildroot/pi2/zImage /tmp/                 
 echo "Copying new Device Tree...."
-sshpass -p "epl@123" scp epl@192.168.0.2:/home/epl/LTIB/LTP_Source/home/epl/LTPbuildroot/pi2/bcm2709-rpi-2-b.dtb /tmp/                 
+sshpass -p "epl@123" scp epl@10.144.172.149:/home/epl/LTIB/LTP_Source/home/epl/LTPbuildroot/pi2/bcm2709-rpi-2-b.dtb /tmp/                 
 echo "Copying new Filesystem..."
-sshpass -p "epl@123" scp epl@192.168.0.2:/home/epl/LTIB/LTP_Source/home/epl/LTPbuildroot/pi2/rootfs.ext4 /tmp/                 
+sshpass -p "epl@123" scp epl@10.144.172.149:/home/epl/LTIB/LTP_Source/home/epl/LTPbuildroot/pi2/rootfs.ext4 /tmp/                 
 
 echo "Verify Checksum..."
 if [[ -f /tmp/zImage && -f /tmp/bcm2709-rpi-2-b.dtb && -f /tmp/rootfs.ext4 ]];then 
