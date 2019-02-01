c8c079742baa155d325f7df6f6ba79a545bf5e7d
purushotham447 <srinivas_r@hcl.com>
2019-02-01 11:58:24 +0530

    Updated profile scripts

--- Git_Commands.sh
+++ Git_Commands.sh
@@ -3,7 +3,7 @@ git status
 git add .gitattributes
 git add . --force
 git status
-git commit -m "Removed I2C test case"
+git commit -m "Updated profile scripts"
 git status
 git remote -v
 git push -u origin master
