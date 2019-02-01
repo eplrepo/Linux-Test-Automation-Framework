4c47eafa4eb1d1cd99bdb80df2be83ba94cfbe31
purushotham447 <srinivas_r@hcl.com>
2019-02-01 10:29:00 +0530

    Updated profile script

--- Git_Commands.sh
+++ Git_Commands.sh
@@ -3,7 +3,7 @@ git status
 git add .gitattributes
 git add . --force
 git status
-git commit -m "Added I2C and Sensor testcases"
+git commit -m "Updated profile script"
 git status
 git remote -v
 git push -u origin master
