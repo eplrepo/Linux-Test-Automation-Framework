11d424001cb121f282150efe83b7802828bd5288
epl <purushotham.449@gmail.com>
2019-03-13 10:35:12 +0530

    Updated Scripts

--- Framework/Build-Scripts/NewBinaries.sh
+++ Framework/Build-Scripts/NewBinaries.sh
@@ -1,11 +1,10 @@
 echo "Downloading New Software from Host to target..."
 echo "Downloading Kernel Image...."
-sshpass -p "epl@123" scp epl@10.144.172.148:/home/epl/pi2-shared/zImage /tmp/                 
+sshpass -p "epl@123" scp epl@10.144.172.148:/home/epl/POC/Linux-Test-Automation-Framework/Jenkins-Mounted/pi2/zImage /tmp/                 
 echo "Downloading Device Tree...."
-sshpass -p "epl@123" scp epl@10.144.172.148:/home/epl/pi2-shared/bcm2709-rpi-2-b.dtb /tmp/      
+sshpass -p "epl@123" scp epl@10.144.172.148:/home/epl/POC/Linux-Test-Automation-Framework/Jenkins-Mounted/pi2/bcm2709-rpi-2-b.dtb /tmp/      
 echo "Downloading Application..."
-sshpass -p "epl@123" scp epl@10.144.172.148:/home/epl/pi2-shared/rootfs.ext4 /tmp/                 
-
+sshpass -p "epl@123" scp epl@10.144.172.148:/home/epl/POC/Linux-Test-Automation-Framework/Jenkins-Mounted/pi2/rootfs.ext4 /tmp/                 
 echo "Verify Checksum..."
 if [[ -f /tmp/zImage && -f /tmp/bcm2709-rpi-2-b.dtb && -f /tmp/rootfs.ext4 ]];then 
 		echo "Checksum Passed..."
